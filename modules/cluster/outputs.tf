8d0f063f5b0161255bfcf8fbdc025df8cf0703c6
Guilherme Crocetti <guilherme.crocetti@secomind.com>
2024-11-12 12:11:29 +0100

    Update(modules): Add cluster_server and cluster_ca_data outputs for GKE cluster
    Signed-off-by: Guilherme Crocetti <guilherme.crocetti@secomind.com>

--- modules/cluster/outputs.tf
+++ modules/cluster/outputs.tf
@@ -12,3 +12,15 @@ output "service_account_email" {
   description = "service account email used for the cluster nodes"
   value       = google_service_account.argocd-k8s.email
 }
+
+output "cluster_server" {
+  description = "endpoint of the cluster"
+  value       = google_container_cluster.primary.endpoint
+  sensitive   = true
+}
+
+output "cluster_ca_data" {
+  description = "Base64 encoded public certificate that is the root certificate of the cluster."
+  value       = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
+  sensitive   = true
+}
